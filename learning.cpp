6c903d1f0ea6babfc34f09cba2971e18e1390dcf
Chenxi Li <spiritlcx@gmail.com>
2015-10-22 19:50:31 +0200

    first commit

---
+++ learning.cpp
@@ -0,0 +1,7 @@
+
+//Avoid casting! Only use casting if necessary!
+//no casting if design is good
+int main(){
+	int a = static_cast<int>(5.0);
+	int b = dynamic_cast<int>(6.0);
+}
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	cout << "Hello World\n";
+	vector<int> k;
+}
+
